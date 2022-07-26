57c414b6d6b969e353c9005b8d1a417c852d34c5
Elena <malkovva@gmail.com>
2022-07-26 10:58:29 +0300

    Added solution to Task 1

---
+++ Task001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите положительное целое число А");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите положительное целое число B");
+int b = int.Parse(Console.ReadLine());
+int result = 1;
+
+for (int count = 1; count <= b; count++)
+{
+    result = result * a;
+}
+Console.WriteLine("Число {0} в степени {1} = {2}", a, b, result);
