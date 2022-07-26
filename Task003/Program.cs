a1dfbe222dbef1fdedbb252be7a2f7389a064e42
Elena <malkovva@gmail.com>
2022-07-26 12:25:12 +0300

    Formatted solution to Task 4

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-int [] array = new int[8];
+int[] array = new int[8];
 int i = 0;
 
-while (i<=7)
+while (i <= 7)
 {
-    array[i] = new Random().Next(0,10000);
+    array[i] = new Random().Next(0, 10000);
     i++;
 }
 
-Console.WriteLine("Array [{0}]",  String.Join (" ", array));
+Console.WriteLine("Array [{0}]", String.Join(" ", array));
