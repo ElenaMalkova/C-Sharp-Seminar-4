f312ac50465d8986b9e13f06ec1e4c670bac05d7
Elena <malkovva@gmail.com>
2022-07-26 12:02:49 +0300

    Added solution to Task 2

---
+++ Task002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumOfDigits (string number)
+{
+int summa = 0;
+for (int i = 0; i<number.Length; i++)
+    {
+        int digit = int.Parse(number[i].ToString());
+        summa += digit;
+    }
+    return summa;
+}
+
+Console.WriteLine("Введите целое положительное число:");
+string number = Console.ReadLine();
+Console.WriteLine("Сумма цифр в числе = " + SumOfDigits(number));
